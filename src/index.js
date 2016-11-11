5cb71e442af3d4cad82f4b64b0b3ffd975405da4
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-10 21:55:10 -0800

    Move initialization logic into index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import configureStore from './configureStore'
+import axios from 'axios'
+import { createStore } from 'redux'
 
-const store = configureStore()
+function parseData(data) {
+  const assets = data.data.data
+  console.log(assets)
+  render(assets)
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'RECEIVE_ASSETS':
+
+      return state
+    default:
+      return state
+  }
+}
+
+const initialState = []
+
+const store = createStore(reducer, initialState)
+
+axios.get('http://api.getchute.com/v2/albums/aus6kwrg/assets')
+.then(function (response) {
+  parseData(response)
+})
+.catch(function (error) {
+  console.log(error)
+})
 
 function render (assets) {
   ReactDOM.render(
