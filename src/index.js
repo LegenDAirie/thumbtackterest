6edf63d656567f1302ff378a985984a271505196
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-10 21:39:10 -0800

    Create basic reducer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import configureStore from './configureStore'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'RECEIVE_ASSETS':
+
+      return state
+    default:
+      return state
+  }
+}
+
+const store = configureStore()
 
 function render (assets) {
   ReactDOM.render(
