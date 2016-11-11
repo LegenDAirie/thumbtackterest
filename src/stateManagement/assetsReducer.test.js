b8a9247cb6ec8e0ddc0178fc5376a8cd61822db9
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-11 14:07:54 -0800

    Test for state mutation

--- src/stateManagement/assetsReducer.test.js
+++ src/stateManagement/assetsReducer.test.js
@@ -1,4 +1,5 @@
 import assetsReducer from './assetsReducer'
+import deepFreeze from 'deep-freeze'
 
 describe('assets Reducer', () => {
 
@@ -16,6 +17,22 @@ describe('assets Reducer', () => {
     const stateBefore = undefined
     const action = {}
     const stateAfter = []
+
+    expect(
+      assetsReducer(stateBefore, action)
+    ).toEqual(stateAfter)
+  })
+
+  it('does not mutate the state', () => {
+    const stateBefore = [1, 2, 3]
+    const action = {
+      type: 'RECEIVE_ASSETS',
+      assets: [4, 5, 6]
+    }
+    const stateAfter = [1, 2, 3, 4, 5, 6]
+
+    deepFreeze(stateBefore)
+    deepFreeze(action)
 
     expect(
       assetsReducer(stateBefore, action)
