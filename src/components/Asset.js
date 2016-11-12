d0c129b6e4f075bd25ae27676e440cd4b29519c3
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-12 15:52:28 -0800

    Add more asset information

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Asset = ({ thumbnail, caption }) => {
-
+const Asset = ({ thumbnail, caption, user, tags, hearts }) => {
+  console.log(thumbnail)
   return (
     <div>
       <img
@@ -10,6 +10,23 @@ const Asset = ({ thumbnail, caption }) => {
         width='100'
         height='100'
       />
+      <img
+        src={ user.avatar }
+        alt="users avatar image"
+      />
+      from { user.username }
+      <ul>
+        { tags.map(function (tag) {
+          return <li key={ tag }>{ tag }</li>
+        })}
+      </ul>
+      { caption }
+      ❤️ { hearts }
+      <img
+        src={ user.avatar }
+        alt="users avatar image"
+      />
+      { user.name }
     </div>
   )
 }
