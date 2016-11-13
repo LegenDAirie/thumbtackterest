c955db8d727b2d46d53235b27fd4ab6f01fee69a
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-13 15:36:42 -0800

    Make action creators

---
+++ src/stateManagement/actionCreators.js
@@ -0,0 +1,9 @@
+export const receiveAssets = (assets) => ({
+  type: 'RECEIVE_ASSETS',
+  assets
+})
+
+export const setLightBoxAsset = (assetID) => ({
+  type: 'SET_LIGHT_BOX_ASSET',
+  assetID
+})
