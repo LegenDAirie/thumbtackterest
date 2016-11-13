62b23ffdb785c62e0d30359830fc664e6f699a9d
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-12 18:11:44 -0800

    Clean up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Asset from './components/Asset'
 import './App.css';
 
-  const App = ({ assets }) => (
-  <div className='grid'>
-    { assets.map( asset =>
-      <Asset key={ asset.id }{ ...asset }/>
-    )}
-  </div>
-)
+const App = ({ assets }) => {
+
+  return (
+    <div className='grid'>
+      { assets.map( asset =>
+        <Asset key={ asset.id } { ...asset }/>
+      )}
+    </div>
+  )
+}
 
 export default App;
