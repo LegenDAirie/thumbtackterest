b7d73491d7381fdc2506ba8fa60f70b1ae15be03
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-13 14:43:45 -0800

    Center lightbox image

--- src/LightBox.js
+++ src/LightBox.js
@@ -5,7 +5,7 @@ const LightBox = ({ shortcut, album_id, caption, dimensions }) => {
   return (
     <div>
       <div className='light-box-shadow'></div>
-      <div className='light-box-image-container'>
+        <div className='light-box-image-container'>
         <div className='light-box'>
           <Image shortcut={ shortcut } album={ album_id } alt={ caption } maxWidth={ dimensions.width }/>
         </div>
