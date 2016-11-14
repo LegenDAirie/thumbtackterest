4aea22fe7fe324d0ec66b6883e7232c19b649f8e
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-13 19:36:33 -0800

    Pull asset query logic into an action creator

--- src/index.js
+++ src/index.js
@@ -2,26 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import axios from 'axios'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import appReducer from './stateManagement/appReducer'
+import { getAssets } from './stateManagement/actionCreators'
 
 const store = createStore(appReducer)
 
-function parseData(data) {
-  const assets = data.data.data
-
-  store.dispatch({ type: 'RECEIVE_ASSETS', assets })
-}
-
-axios.get('http://api.getchute.com/v2/albums/aus6kwrg/assets?type=image&per_page=10')
-.then(function (response) {
-  parseData(response)
-})
-.catch(function (error) {
-  console.log('cats', error)
-})
+getAssets(store.dispatch)
 
 ReactDOM.render(
   <Provider store={ store }>
--- src/stateManagement/actionCreators.js
+++ src/stateManagement/actionCreators.js
@@ -1,9 +1,21 @@
-export const receiveAssets = (assets) => ({
-  type: 'RECEIVE_ASSETS',
-  assets
-})
+import axios from 'axios'
 
 export const setLightBoxAsset = (assetID) => ({
   type: 'SET_LIGHT_BOX_ASSET',
   assetID
 })
+
+const receiveAssets = (assets) => ({
+  type: 'RECEIVE_ASSETS',
+  assets
+})
+
+export const getAssets = (dispatch) => {
+  axios.get('http://api.getchute.com/v2/albums/aus6kwrg/assets?type=image&per_page=10')
+  .then(function (response) {
+    dispatch(receiveAssets(response.data.data))
+  })
+  .catch(function (error) {
+    console.log('cats', error)
+  })
+}
