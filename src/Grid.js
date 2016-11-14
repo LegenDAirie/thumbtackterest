6911fd973378e27d9ceabc13992d0efab6d4a642
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-14 15:21:41 -0800

    Wrap Grid component with InfiniteScroll

--- src/Grid.js
+++ src/Grid.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Asset from './components/Asset'
 import { connect } from 'react-redux'
+import InfiniteScroll from 'react-infinite-scroller'
+import { getAssets } from './stateManagement/actionCreators'
 
-const Grid = ({ assetIDs, assets }) => {
+const Grid = ({ assetIDs, assets, dispatch }) => {
 
   return (
-    <div className='grid'>
-      { assetIDs.map( assetID =>
-        <Asset key={ assetID } { ...assets[assetID] }/>
-      )}
-    </div>
+    <InfiniteScroll
+      pageStart={ 0 }
+      hasMore={ true }
+      loadMore={ () => {
+        var lastAssetID = assetIDs[assetIDs.length - 1]
+        var url = `http://api.getchute.com/v2/albums/aus6kwrg/assets?max_id=${lastAssetID}&per_page=10&type=image`
+        getAssets(dispatch, url)
+      } }
+    >
+      <div className='grid'>
+        { assetIDs.map( assetID =>
+          <Asset key={ assetID } { ...assets[assetID] }/>
+        )}
+      </div>
+    </InfiniteScroll>
   )
 }
 
