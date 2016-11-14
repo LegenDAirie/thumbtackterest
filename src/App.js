b21e7e156aef31b5a149851d1e1b902c0d9e937f
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-13 19:17:22 -0800

    Clean up

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import LightBox from './LightBox'
 import './App.css';
 
 const App = ({ state }) => {
-  const { assets, currentLightBoxImageID } = state
   return (
     <div>
-      <Header/>
-      <LightBox shouldRender={ currentLightBoxImageID }
-        { ...assets[currentLightBoxImageID] }
-      />
-      <Grid assetIDs={ assets.assetIDs } assets={ assets.assets } />
+      <Header />
+      <LightBox />
+      <Grid />
     </div>
   )
 }
