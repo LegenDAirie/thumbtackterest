da3a0731f4adcf3a8bd71c520baeee45164812c0
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-13 16:09:47 -0800

    Update assetReducer to accept new state structure

--- src/stateManagement/assetsReducer.js
+++ src/stateManagement/assetsReducer.js
@@ -1,10 +1,32 @@
-const assetsReducer = (state = [], action) => {
+const combineAssetIDs = (currentIDs = [], newAssetList = []) => {
+  const newAssetIDs = newAssetList.map( asset => asset.id)
+
+  return [
+    ...currentIDs,
+    ...newAssetIDs
+  ]
+}
+
+const hashNewAssets = (currentAssets = {}, newAssetList = []) => {
+  const newAssets = newAssetList.reduce( (accumulation, asset) => {
+    return Object.assign({}, accumulation, { [asset.id]: asset })
+  }, {})
+  return Object.assign({}, currentAssets, newAssets)
+}
+
+const initialState = {
+  assetIDs: [],
+  assets: {}
+}
+
+const assetsReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'RECEIVE_ASSETS':
-      return [
-        ...state,
-        ...action.assets
-      ]
+    console.log()
+      return Object.assign({}, state, {
+        assetIDs: combineAssetIDs(state.assetIDs, action.assets),
+        assets: hashNewAssets(state.assets, action.assets)
+      })
 
     default:
       return state
