f592decbc1d54001e692fec3ade122c258161c32
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-14 15:26:35 -0800

    Clean up syntax

--- src/Grid.js
+++ src/Grid.js
@@ -4,33 +4,27 @@ import { connect } from 'react-redux'
 import InfiniteScroll from 'react-infinite-scroller'
 import { getAssets } from './stateManagement/actionCreators'
 
-const Grid = ({ assetIDs, assets, dispatch }) => {
+const Grid = ({ assetIDs, assets, dispatch }) => (
+  <InfiniteScroll
+    className='grid'
+    pageStart={ 0 }
+    hasMore={ true }
+    loadMore={ () => {
+      var lastAssetID = assetIDs[assetIDs.length - 1]
+      var url = `http://api.getchute.com/v2/albums/aus6kwrg/assets?max_id=${lastAssetID}&per_page=10&type=image`
+      getAssets(dispatch, url)
+    }}
+  >
+    { assetIDs.map( assetID =>
+      <Asset key={ assetID } { ...assets[assetID] }/>
+    )}
+  </InfiniteScroll>
+)
 
-  return (
-    <InfiniteScroll
-      pageStart={ 0 }
-      hasMore={ true }
-      loadMore={ () => {
-        var lastAssetID = assetIDs[assetIDs.length - 1]
-        var url = `http://api.getchute.com/v2/albums/aus6kwrg/assets?max_id=${lastAssetID}&per_page=10&type=image`
-        getAssets(dispatch, url)
-      } }
-    >
-      <div className='grid'>
-        { assetIDs.map( assetID =>
-          <Asset key={ assetID } { ...assets[assetID] }/>
-        )}
-      </div>
-    </InfiniteScroll>
-  )
-}
 
-const mapStateToProps = (state) => {
-  const { assetIDs, assets } = state.assets
-  return {
-    assetIDs,
-    assets
-  }
-}
+const mapStateToProps = (state) => ({
+  assetIDs: state.assets.assetIDs,
+  assets: state.assets.assets
+})
 
 export default connect(mapStateToProps)(Grid)
